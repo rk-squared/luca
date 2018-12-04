6621255d6075dd39393a4b6172ce1c47bea90b0a
Josh Kelley <joshkel@gmail.com>
2018-12-03 22:18:06 -0500

    Update comments; remove fields that don't properly apply
    The code converts "abilities," but JSON abilities are lower-level than
    what the game calls abilities, so rarity, orbs, etc.  don't apply.

--- src/battleActions.ts
+++ src/battleActions.ts
@@ -201,6 +201,10 @@ const toBool = (value: string | number) => !!+value;
 const toBoolOrNull = (value: string | number | undefined) => (value == null ? null : toBool(value));
 const msecToSec = (msec: string | number) => +msec / 1000;
 
+/**
+ * High-level function to convert JSON for a game "ability" (including soul
+ * break or magicite skill) to JSON.
+ */
 export function convertAbility(
   battleData: BattleData,
   abilityData: BuddyAbility | BuddySoulStrike | BeastActiveSkill,
@@ -238,11 +242,17 @@ export function convertAbility(
   // Omit options.target_death; it corresponds to TARGET_DEATH, but abilities'
   // effects make it obvious whether they can target dead allies.
 
+  // Not included: rarity, uses, max, orbs
+  // Those apply to what the game UI calls abilities, but what the game UI
+  // calls abilities is not the same as what the game JSON calls abilities.
+
+  // Also not included: nameJp, gl, introducingEvent
+  // Those need to be handled at a higher level and likely require outside
+  // data and/or human intervention.
   return {
     school,
     name: options.name.trim(),
     alias: alias ? alias.trim() : alias,
-    rarity: toDo,
     type: battleData.damageTypeLookup[+abilityData.exercise_type],
     target: battleData.describeTarget(
       options.target_range,
@@ -257,13 +267,7 @@ export function convertAbility(
     counter: toBoolOrNull(options.counter_enable),
     autoTarget: toDo,
     sb: options.ss_point == null ? null : +options.ss_point,
-    uses: toDo,
-    max: toDo,
-    orbs: toDo,
-    introducingEvent: toDo,
-    nameJp: toDo,
     id: +abilityData.ability_id,
-    gl: true,
     action: action ? action.className : null,
     args: getNamedArgs(battleData, actionLookup, +abilityData.action_id, options),
   };
